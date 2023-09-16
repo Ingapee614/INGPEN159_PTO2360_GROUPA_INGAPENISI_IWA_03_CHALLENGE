59d373e4c6a90ac4c98c0d49c10de454656d08c6
Ingapee614 <132918785+Ingapee614@users.noreply.github.com>
2023-09-16 19:43:47 +0200

    Challenge1-2( script, import,export)

---
+++ scripts.js
@@ -0,0 +1,5 @@
+// scripts.js
+import{role as NwabisaRole}from "./nwabisa.js"
+import { role as JohannesRole } from "./johannes.js"
+import{role as AlexRole} from "./alex.js"
+console.log('Roles:',NwabisaRole,"," ,JohannesRole,",",AlexRole)
